c6480e94b11c8303d0435fada0bc4358923aaea1
Diego <dsimog01@estudiantes.unileon.es>
2022-09-23 13:12:01 +0200

    Added API call to get validator info

---
+++ ui/src/apis/beaconchaApi/types.ts
@@ -0,0 +1,21 @@
+export interface BeaconchaGetResponse {
+  status: string;
+  data: {
+    activationeligibilityepoch?: number;
+    activationepoch?: number;
+    balance?: number;
+    effectivebalance?: number;
+    exitepoch?: number;
+    lastattestationslot?: number;
+    name?: string;
+    pubkey?: string;
+    slashed?: boolean;
+    status?: string;
+    validatorindex?: number;
+    withdrawableepoch?: number;
+    withdrawalcredentials?: string;
+  };
+  error?: { message: string };
+}
+
+//TODO: is it OK to have all optional fields?
