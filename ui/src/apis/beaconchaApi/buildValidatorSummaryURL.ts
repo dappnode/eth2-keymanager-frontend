785d3da091ad59a31e9dcd83117b0e43bab69eec
Diego <dsimog01@estudiantes.unileon.es>
2022-09-23 13:39:25 +0200

    Added funct. to build 1 URL for several validators

---
+++ ui/src/apis/beaconchaApi/buildValidatorSummaryURL.ts
@@ -0,0 +1,21 @@
+import { BeaconchaGetResponse } from "./types";
+import { beaconchaApiParamsMap } from "../../params";
+
+export default function buildValidatorSummaryURL({
+  allValidatorsInfo,
+  network,
+}: {
+  allValidatorsInfo: BeaconchaGetResponse[];
+  network: string;
+}): string {
+  let summaryValidatorURL =
+    beaconchaApiParamsMap.get(network)!.baseUrl + "/dashboard?validators="; //TODO Be careful with the !
+
+  allValidatorsInfo.forEach((validatorInfo) => {
+    summaryValidatorURL += validatorInfo.data.validatorindex + ",";
+  });
+
+  summaryValidatorURL = summaryValidatorURL.slice(0, -1);
+
+  return summaryValidatorURL;
+}
