38370e84cbd513f057b55d13c4d736480b30b7d3
Diego <dsimog01@estudiantes.unileon.es>
2022-09-23 13:31:18 +0200

    Added function to get validator info from their PK

--- ui/src/apis/beaconchaApi/index.ts
+++ ui/src/apis/beaconchaApi/index.ts
@@ -1,5 +1,6 @@
 import { BeaconchaGetResponse } from "./types";
 import { ApiParams } from "../../types";
+import { Web3signerGetResponse } from "../web3signerApi/types";
 
 //TODO Use inheritance for both APIs?
 export class BeaconchaApi {
@@ -13,11 +14,36 @@ export class BeaconchaApi {
     this.fullUrl = this.baseUrl + this.endpoint;
   }
 
+  /*
+   * Fetch info for every validator PK
+   */
+  public async fetchAllValidatorsInfo({
+    beaconchaApi,
+    keystoresGet,
+  }: {
+    beaconchaApi: BeaconchaApi;
+    keystoresGet: Web3signerGetResponse;
+  }): Promise<BeaconchaGetResponse[]> {
+    const validatorsInfo = new Array<BeaconchaGetResponse>();
+    let allValidatorPKs = keystoresGet.data.map(
+      (keystoreData) => keystoreData.validating_pubkey
+    );
+
+    for await (const validatorPK of allValidatorPKs) {
+      const validatorInfo = await beaconchaApi.fetchValidatorInfo(validatorPK);
+      validatorsInfo.push(validatorInfo);
+    }
+
+    return validatorsInfo;
+  }
+
   /**
    * Get validator indexes for a list of public keys
    * https://beaconcha.in/api/v1/docs/index.html#/Validator/get_api_v1_validator__indexOrPubkey_
    */
-  public async getValidatorInfo(pubkey: string): Promise<BeaconchaGetResponse> {
+  public async fetchValidatorInfo(
+    pubkey: string
+  ): Promise<BeaconchaGetResponse> {
     try {
       return (await this.request(
         "GET",
