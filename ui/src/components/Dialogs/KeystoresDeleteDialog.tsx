702ea1b778beaa3bfc2fb7c33aba652ba9b1cffc
Diego <dsimog01@estudiantes.unileon.es>
2022-09-27 17:18:57 +0200

    Extracted deletion warnings to new file

--- ui/src/components/Dialogs/KeystoresDeleteDialog.tsx
+++ ui/src/components/Dialogs/KeystoresDeleteDialog.tsx
@@ -18,6 +18,7 @@ import {
 } from "../../apis/web3signerApi/types";
 import { importDialogBoxStyle } from "../../Styles/dialogStyles";
 import WaitBox from "../WaitBox/WaitBox";
+import DeletionWarning from "./DeletionWarning";
 
 export default function KeystoresDeleteDialog({
   web3signerApi,
@@ -110,24 +111,7 @@ export default function KeystoresDeleteDialog({
                 <WaitBox />
               ) : (
                 <DialogContentText id="alert-dialog-description">
-                  Are you sure you want to delete these keystores?
-                  <ul>
-                    {selectedRows.map((row, i) => (
-                      <li key={i}>
-                        {shortenPubkey(
-                          rows[parseInt(row.toString())].validating_pubkey
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                  After deletion, these keystores won't be used for signing
-                  anymore and your slashing protection data will be downloaded.{" "}
-                  <br />
-                  <br />
-                  <b>
-                    Keep the slashing protection data for when you want to
-                    import these keystores to a new validator.
-                  </b>
+                  <DeletionWarning rows={rows} selectedRows={selectedRows} />
                 </DialogContentText>
               )}
             </div>
