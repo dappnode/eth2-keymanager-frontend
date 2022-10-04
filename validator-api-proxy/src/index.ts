69c3a1e8e3fa3e17ccd7ef76837e99e442bd8eed
Diego <dsimog01@estudiantes.unileon.es>
2022-10-04 17:50:06 +0200

    Added POST feerecipient functionality to proxy

--- validator-api-proxy/src/index.ts
+++ validator-api-proxy/src/index.ts
@@ -15,6 +15,9 @@ app.use(
   })
 );
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/feerecipient", async (req, res) => {
   //TODO: Handle params not existing
 
@@ -39,6 +42,40 @@ app.get("/feerecipient", async (req, res) => {
     );
 
     res.send(feeRecipientGetOld);
+  }
+});
+
+app.post("/feerecipient", async (req, res) => {
+  //TODO: Handle params not existing
+
+  const client = req.query.client as string;
+  const validatorPublicKey = req.query.validatorPublicKey as string;
+  const newFeeRecipient = req.body?.ethaddress as string;
+
+  if (client === undefined || validatorPublicKey === undefined) {
+    res
+      .status(400)
+      .send("Bad Request. Missing consensus client or validator public key.");
+  } else if (!validatorClientApiMap.has(client)) {
+    res.status(400).send("Bad Request. Invalid consensus client.");
+  } else if (req.body.ethaddress === undefined) {
+    res.status(400).send("Bad Request. Missing fee recipient eth address.");
+  } else {
+    const validatorApi = new ValidatorClientRequester(
+      validatorClientApiMap.get(client)!
+    );
+
+    //TODO Perform validator public key checks
+
+    await validatorApi
+      .setFeeRecipient(newFeeRecipient, validatorPublicKey)
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+
+    //TODO: Perform GET to check if address has been set?
+
+    res.send("Fee recipient set successfully.");
   }
 });
 
