13d02de979aa56942f54dc22892264d566441a99
Diego <dsimog01@estudiantes.unileon.es>
2022-10-19 19:02:35 +0200

    Removed hardcoded params

--- ui/src/params.ts
+++ ui/src/params.ts
@@ -1,8 +1,6 @@
 import { ApiParams } from "./types";
 import { env } from "./env";
 
-const isProduction = env.NODE_ENV === "production";
-
 export const beaconchaApiParamsMap = new Map<string, ApiParams>([
   ["mainnet", { baseUrl: "https://beaconcha.in", apiPath: "/api/v1/" }],
   ["prater", { baseUrl: "https://prater.beaconcha.in", apiPath: "/api/v1/" }],
@@ -22,16 +20,10 @@ export const availableNetworks = ["mainnet", "prater", "gnosis"];
 
 export const maxValidatorsPerRequest = 100; //For beaconcha.in --> TODO: is it the same for Gnosis?
 
-export const network = isProduction ? env.REACT_APP_NETWORK || "" : "prater";
+export const network = env.REACT_APP_NETWORK || "";
 
-export const consensusClient = isProduction
-  ? env.REACT_APP_CONSENSUS_CLIENT || ""
-  : "prysm";
+export const consensusClient = env.REACT_APP_CONSENSUS_CLIENT || "";
 
-export const executionClient = isProduction
-  ? env.REACT_APP_EXECUTION_CLIENT || ""
-  : "geth";
+export const executionClient = env.REACT_APP_EXECUTION_CLIENT || "";
 
-export const web3signerApiURL = isProduction
-  ? env.REACT_APP_WEB3SIGNER_API_URL || ""
-  : "http://web3signer.web3signer-prater.dappnode:9000";
+export const web3signerApiURL = env.REACT_APP_WEB3SIGNER_API_URL || "";
